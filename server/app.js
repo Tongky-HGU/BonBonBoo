b585617a8f529d9f77415f11e3899f230152b38f
chosh <chotjd329@hotmail.com>
2021-08-20 11:41:24 +0900

    [feature] server log & cors library

--- server/app.js
+++ server/app.js
@@ -3,9 +3,19 @@ const config = require("./config/config");
 const express = require("express");
 const db = require("./services/db");
 const bodyParser = require("body-parser");
+const cors = require("cors")
+const morgan = require("morgan")
 
 const app = express();
 const port = config.port;
+
+
+//*log
+// app.use(morgan('combined'));
+app.use(morgan('dev'));
+
+//* cors
+app.use(cors())
 
 //* Static File service
 app.use(express.static("public"));
@@ -15,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //* Routes
-app.use("/stock", require("./routes/stock-route"));
-app.use("/exchange", require("./routes/exchange-route"));
+app.use("/api/stock", require("./routes/stock-route"));
+app.use("/api/exchange", require("./routes/exchange-route"));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
